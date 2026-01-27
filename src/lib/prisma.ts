e8c27bc9cf292482160a0033134831a5e66433b8
Alejandro Cortes <alejandro.cs389@gmail.com>
2026-01-27 15:38:27 -0600

    se reparo configuracion de prisma y se removio paquete innecesario

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 
 const connectionString = process.env.DATABASE_URL;
@@ -11,6 +11,7 @@ if (!connectionString) {
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
